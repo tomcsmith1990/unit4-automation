835c9843e59459447197c3d115f98ae9a4db14f3
Tom Smith <tom.smith@cambridgeshire.gov.uk>
2018-05-09 15:08:18 +0100

    Extract class so that Program is just a simple runner

---
+++ Unit4/Unit4/ReportRunner.cs
@@ -0,0 +1,30 @@
+using System;
+using System.IO;
+
+namespace Unit4
+{
+    internal class ReportRunner
+    {
+        public void Run()
+        {
+            try
+            {
+                var credentials = new Credentials();
+
+                var inputFile = Path.Combine(Directory.GetCurrentDirectory(), "Vol Orgs BCR.rerx");
+                var outputFile = Path.Combine(Directory.GetCurrentDirectory(), "Vol Orgs BCR.xlsx");
+
+                var engine = new Unit4Engine(credentials);
+                engine.RunReport(inputFile, outputFile);
+                
+                Console.WriteLine("Success");
+            }
+            catch (Exception e) {
+                Console.WriteLine(e.Message);
+                Console.WriteLine();
+                Console.WriteLine(e.GetType());
+                Console.WriteLine(e.StackTrace);
+            }
+        }
+    }
+}
