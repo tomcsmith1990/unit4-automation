cdb2cd8c3a93c1b685ffc1e183dcd141065d82a4
Tom Smith <tom.smith@cambridgeshire.gov.uk>
2018-05-09 10:17:08 +0100

    Run an RERX file given a username, password, client &  SOAP url

---
+++ Unit4/Unit4/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using ReportEngine;
+using ReportEngine.Base.Data.Provider;
+using ReportEngine.Interfaces;
+using ReportEngine.Base.Security;
+using ReportEngine.Base.Interfaces;
+using ReportEngine.Provider.WebService;
+using System.Data;
+using ReportEngine.IO;
+using ReportEngine.Data;
+using System.IO;
+
+namespace Unit4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var username = "";
+            var password = "";
+            var soapService = "";
+            var client = "";
+
+            var inputFile = Path.Combine(Directory.GetCurrentDirectory(), "Vol Orgs BCR.rerx");
+            var outputFile = Path.Combine(Directory.GetCurrentDirectory(), "Vol Orgs BCR.xlsx");
+
+            var agressoAuthenticator = new AgressoAuthenticator();
+            agressoAuthenticator.Password = SecureStringHelper.ToSecureString(password);
+            
+            var authenticators = new BaseAuthenticator[1]
+            {
+                agressoAuthenticator
+            };
+            var connector = new WebProviderConnector() 
+            { 
+                Name = "WebService",
+                Authenticators = authenticators,
+                Authenticator = agressoAuthenticator,
+                Datasource = soapService
+            };
+            
+            agressoAuthenticator.Name = username;
+            agressoAuthenticator.Client = client;
+
+            var engine = new Engine(new WebProvider(connector), ClientType.External);
+            engine.InProcess = true;
+            using (engine)
+            {
+                DataSet dataset = null;
+                engine.RunReportEx(inputFile, outputFile, false);
+            }
+        }
+    }
+}
