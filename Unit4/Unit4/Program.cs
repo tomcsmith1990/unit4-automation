e8d13c29e02298ed8a80cbfe0d57b8c0320cad73
Tom Smith <tom.smith@cambridgeshire.gov.uk>
2018-05-09 10:47:31 +0100

    Catch all errors to stop an unhandled exception crash

--- Unit4/Unit4/Program.cs
+++ Unit4/Unit4/Program.cs
@@ -16,38 +16,47 @@ namespace Unit4
     {
         static void Main(string[] args)
         {
-            var username = "";
-            var password = "";
-            var soapService = "";
-            var client = "";
+            try {
+                var username = "";
+                var password = "";
+                var soapService = "";
+                var client = "";
 
-            var inputFile = Path.Combine(Directory.GetCurrentDirectory(), "Vol Orgs BCR.rerx");
-            var outputFile = Path.Combine(Directory.GetCurrentDirectory(), "Vol Orgs BCR.xlsx");
+                var inputFile = Path.Combine(Directory.GetCurrentDirectory(), "Vol Orgs BCR.rerx");
+                var outputFile = Path.Combine(Directory.GetCurrentDirectory(), "Vol Orgs BCR.xlsx");
 
-            var agressoAuthenticator = new AgressoAuthenticator();
-            agressoAuthenticator.Password = SecureStringHelper.ToSecureString(password);
-            
-            var authenticators = new BaseAuthenticator[1]
-            {
-                agressoAuthenticator
-            };
-            var connector = new WebProviderConnector() 
-            { 
-                Name = "WebService",
-                Authenticators = authenticators,
-                Authenticator = agressoAuthenticator,
-                Datasource = soapService
-            };
-            
-            agressoAuthenticator.Name = username;
-            agressoAuthenticator.Client = client;
+                var agressoAuthenticator = new AgressoAuthenticator();
+                agressoAuthenticator.Password = SecureStringHelper.ToSecureString(password);
+                
+                var authenticators = new BaseAuthenticator[1]
+                {
+                    agressoAuthenticator
+                };
+                var connector = new WebProviderConnector() 
+                { 
+                    Name = "WebService",
+                    Authenticators = authenticators,
+                    Authenticator = agressoAuthenticator,
+                    Datasource = soapService
+                };
+                
+                agressoAuthenticator.Name = username;
+                agressoAuthenticator.Client = client;
 
-            var engine = new Engine(new WebProvider(connector), ClientType.External);
-            engine.InProcess = true;
-            using (engine)
-            {
-                DataSet dataset = null;
-                engine.RunReportEx(inputFile, outputFile, false);
+                var engine = new Engine(new WebProvider(connector), ClientType.External);
+                engine.InProcess = true;
+                using (engine)
+                {
+                    DataSet dataset = null;
+                    engine.RunReportEx(inputFile, outputFile, false);
+                    Console.WriteLine("Success");
+                }
+            }
+            catch (Exception e) {
+                Console.WriteLine(e.Message);
+                Console.WriteLine();
+                Console.WriteLine(e.GetType());
+                Console.WriteLine(e.StackTrace);
             }
         }
     }
