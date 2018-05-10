f6d9b8857e5ac7d6468a5f3e7b920be407993a3c
Tom Smith <tom.smith@cambridgeshire.gov.uk>
2018-05-10 11:23:38 +0100

    Pull out a static class for Resql queries

---
+++ Unit4/Unit4/Resql.cs
@@ -0,0 +1,31 @@
+using System;
+
+namespace Unit4
+{
+    internal static class Resql
+    {
+        public static string BcrByCostCentre
+        {
+            get
+            {
+                return @".name [GL-BAL-001 : General Balances Monitoring Report]
+
+.declare [Directorate (Tier1)] String ''
+
+.declare [Service Group (Tier2)] String ''
+
+.declare [Budget Group (Tier4)] String ''
+
+.declare [Cost Centre] String '{0}'
+
+.declare [Service (Tier3)] String ''
+
+.declare [Account] String ''
+
+.query [GL-BAL-001 : General Balances Monitoring Report] 
+    agr_getBrowser 'GL-BAL-001 : General Balances Monitoring Report', r0r0r0r3dim2_eq='$?[Directorate (Tier1)]', r0r0r3dim2_eq='$?[Service Group (Tier2)]', r3dim2_eq='$?[Budget Group (Tier4)]', dim2_eq='$?[Cost Centre]', r0r3dim2_eq='$?[Service (Tier3)]', dim1_eq='$?[Account]'
+.endQuery";
+            }
+        }
+    }
+}
--- Unit4/Unit4/Unit4Engine.cs
+++ Unit4/Unit4/Unit4Engine.cs
@@ -13,23 +13,6 @@ namespace Unit4
 {
     internal class Unit4Engine
     {
-        const string _resql = @".name [GL-BAL-001 : General Balances Monitoring Report]
-
-.declare [Directorate (Tier1)] String ''
-
-.declare [Service Group (Tier2)] String ''
-
-.declare [Budget Group (Tier4)] String ''
-
-.declare [Cost Centre] String '{0}'
-
-.declare [Service (Tier3)] String ''
-
-.declare [Account] String ''
-
-.query [GL-BAL-001 : General Balances Monitoring Report] 
-    agr_getBrowser 'GL-BAL-001 : General Balances Monitoring Report', r0r0r0r3dim2_eq='$?[Directorate (Tier1)]', r0r0r3dim2_eq='$?[Service Group (Tier2)]', r3dim2_eq='$?[Budget Group (Tier4)]', dim2_eq='$?[Cost Centre]', r0r3dim2_eq='$?[Service (Tier3)]', dim1_eq='$?[Account]'
-.endQuery";
         private readonly ICredentials m_Credentials;
 
         public Unit4Engine(ICredentials credentials)
@@ -48,7 +31,7 @@ namespace Unit4
             {
                 var resqlProcessor = new ReSqlProcessor(engine);
 
-                return engine.RunReSql(resqlProcessor, string.Format(_resql, costCentre));
+                return engine.RunReSql(resqlProcessor, string.Format(Resql.BcrByCostCentre, costCentre));
             }
         }
     }
