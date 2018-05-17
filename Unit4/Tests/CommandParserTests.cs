bd1285c0e39a386102c792acb842dcaa19d43b10
Tom Smith <tom.smith@cambridgeshire.gov.uk>
2018-05-17 16:27:37 +0100

    Red: an unknown command should give help

--- Unit4/Tests/CommandParserTests.cs
+++ Unit4/Tests/CommandParserTests.cs
@@ -14,5 +14,13 @@ namespace Unit4.Automation.Tests
 
             Assert.That(commandParser.GetCommand(), Is.EqualTo(CommandParser.Command.Help));
         }
+
+        [Test]
+        public void GivenAnUnknownCommand_ThenTheCommandShouldBeHelp()
+        {
+            var commandParser = new CommandParser();
+
+            Assert.That(commandParser.GetCommand("unknown"), Is.EqualTo(CommandParser.Command.Help));
+        }
     }
 }
