17a6c9b11a6e7a85a94a1ff8e45e2633a57c7736
Tom Smith <tom.smith@cambridgeshire.gov.uk>
2018-05-17 15:02:10 +0100

    Rename exe

--- build-tools.psm1
+++ build-tools.psm1
@@ -6,15 +6,15 @@ function Build {
     Restore
     msbuild.exe .\Unit4\Unit4.sln
 
-    & ".\Unit4\packages\NUnit.ConsoleRunner.3.8.0\tools\nunit3-console.exe" .\Unit4\Unit4\bin\Debug\Unit4.exe --where="cat == RequiresExcelInstall"
+    & ".\Unit4\packages\NUnit.ConsoleRunner.3.8.0\tools\nunit3-console.exe" .\Unit4\Unit4\bin\Debug\unit4-automation.exe --where="cat == RequiresExcelInstall"
 }
 
 function Test {
-    & ".\Unit4\packages\NUnit.ConsoleRunner.3.8.0\tools\nunit3-console.exe" .\Unit4\Unit4\bin\Debug\Unit4.exe
+    & ".\Unit4\packages\NUnit.ConsoleRunner.3.8.0\tools\nunit3-console.exe" .\Unit4\Unit4\bin\Debug\unit4-automation.exe
 }
 
 function Run {
-    & ".\Unit4\Unit4\bin\Debug\Unit4.exe"
+    & ".\Unit4\Unit4\bin\Debug\unit4-automation.exe"
 }
 
 export-modulemember -function Restore
