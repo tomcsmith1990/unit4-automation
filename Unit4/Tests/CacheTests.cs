00bdc9d59b232d51fd859de80e2d9ccfbeaa24d8
Tom Smith <tom.smith@cambridgeshire.gov.uk>
2018-05-21 16:22:32 +0100

    Check we read from the cache

--- Unit4/Tests/CacheTests.cs
+++ Unit4/Tests/CacheTests.cs
@@ -53,6 +53,23 @@ namespace Unit4.Automation.Tests
             mock.Verify(x => x.Write(obj), Times.Once);
         }
 
+        [Test]
+        public void GivenCleanCache_ThenTheResultShouldBeRead()
+        {
+            var obj = new Foo();
+
+            var mock = new Mock<IFile<Foo>>();
+            mock.Setup(x => x.Exists()).Returns(true);
+            mock.Setup(x => x.IsDirty()).Returns(false);
+            mock.Setup(x => x.Read()).Returns(obj);
+
+            var cache = new Cache<Foo>(() => { throw new NotImplementedException(); }, mock.Object);
+
+            var actual = cache.Fetch();
+
+            Assert.That(actual, Is.EqualTo(obj));
+        }
+
         public class Foo
         {
             public Foo Blah()
