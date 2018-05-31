1d31c5926163e6898e61d0252e708e907eac9cce
Tom Smith <tom.smith@cambridgeshire.gov.uk>
2018-05-31 16:53:56 +0100

    Green: filter the list

--- Unit4/Tests/BcrFilterTests.cs
+++ Unit4/Tests/BcrFilterTests.cs
@@ -3,6 +3,7 @@ using Unit4.Automation;
 using NUnit.Framework;
 using Unit4.Automation.Model;
 using Unit4.Automation.Interfaces;
+using System.Linq;
 
 namespace Unit4.Automation.Tests
 {
@@ -50,14 +51,18 @@ namespace Unit4.Automation.Tests
 
     internal class BcrFilter : IBcrMiddleware
     {
+        private readonly BcrOptions _options;
+
         public BcrFilter(BcrOptions options)
         {
-
+            _options = options;
         }
 
         public Bcr Use(Bcr bcr)
         {
-            throw new NotImplementedException();
+            var newBcr = new Bcr();
+            newBcr.Lines = bcr.Lines.Where(x => x.CostCentre.Tier2.Equals(_options.Tier2)).ToList();
+            return newBcr;
         }
     }
 }
