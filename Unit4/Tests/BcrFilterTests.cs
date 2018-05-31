4879fcce597c1d10f33ca70da99eab2cf417d54f
Tom Smith <tom.smith@cambridgeshire.gov.uk>
2018-05-31 16:55:44 +0100

    Red: no tier 2 option should return everything

--- Unit4/Tests/BcrFilterTests.cs
+++ Unit4/Tests/BcrFilterTests.cs
@@ -47,6 +47,25 @@ namespace Unit4.Automation.Tests
 
             Assert.That(filter.Use(bcr).Lines, Has.Count.EqualTo(1));
         }
+
+        [Test]
+        public void GivenNoTier2Option_ThenAllLinesShouldBeIncluded()
+        {
+            var options = new BcrOptions();
+
+            var filter = new BcrFilter(options);
+
+            var bcr = new Bcr();
+            bcr.Lines = new BcrLine[] {
+                    new BcrLine() {
+                        CostCentre = new CostCentre() {
+                            Tier2 = "tier2"
+                        }
+                    },
+                };
+
+            Assert.That(filter.Use(bcr).Lines, Has.Count.EqualTo(1));
+        }
     }
 
     internal class BcrFilter : IBcrMiddleware
