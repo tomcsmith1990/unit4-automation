86b8047e3fad2d70cbd34afe7923eb17ace9966f
Tom Smith <tom.smith@cambridgeshire.gov.uk>
2018-05-31 15:49:36 +0100

    Green: save the tier 2 option

--- Unit4/Model/BcrOptions.cs
+++ Unit4/Model/BcrOptions.cs
@@ -9,15 +9,17 @@ namespace Unit4.Automation.Model
     [Verb("bcr", HelpText = "Produce a BCR.")]
     internal class BcrOptions : IOptions
     {
+        private readonly string _tier2;
+
         public BcrOptions() : this(null)
         {}
 
         public BcrOptions(string tier2)
         {
-
+            _tier2 = tier2;
         }
 
         [Option(HelpText = "Filter by a tier 2 code.")]
-        public string Tier2 { get { throw new NotImplementedException(); } }
+        public string Tier2 { get { return _tier2; } }
     }
 }
