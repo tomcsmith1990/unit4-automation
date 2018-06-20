d020ac6562b505426c84dfb1685de2a2d8b9c76d
Tom Smith <tom.smith@cambridgeshire.gov.uk>
2018-06-20 16:17:51 +0100

    Remove unused field

--- Unit4/ReportEngine/Unit4WebProvider.cs
+++ Unit4/ReportEngine/Unit4WebProvider.cs
@@ -6,13 +6,10 @@ namespace Unit4.Automation.ReportEngine
 {
     internal class Unit4WebProvider : IDisposable
     {
-        private readonly ICredentials m_Credentials;
         private readonly WebProvider _provider;
 
         public Unit4WebProvider(ICredentials credentials)
         {
-            m_Credentials = credentials;
-
             var connector = new Unit4WebConnector(credentials).Create();
             _provider = new WebProvider(connector);
         }
