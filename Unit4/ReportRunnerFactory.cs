faf97b4d35ab2fde37c342b3cb87bd329bbe45c9
Tom Smith <tom.smith@cambridgeshire.gov.uk>
2018-06-27 15:08:38 +0100

    Extract method

--- Unit4/ReportRunnerFactory.cs
+++ Unit4/ReportRunnerFactory.cs
@@ -8,30 +8,40 @@ namespace Unit4.Automation
 {
     internal class ReportRunnerFactory
     {
-        public IRunner Create(IOptions options)
+        private readonly ConfigOptionsFile _file;
+
+        public ReportRunnerFactory()
         {
             var configPath = Path.Combine(Directory.GetCurrentDirectory(), "config.json");
-            var file = new ConfigOptionsFile(configPath);
-            
+            _file = new ConfigOptionsFile(configPath);
+        }
+
+        public IRunner Create(IOptions options)
+        { 
             var bcrOptions = options as BcrOptions;
             if (bcrOptions != null)
             {
-                var log = new Logging();
-                var config = file.Exists() ? file.Load() : new ConfigOptions();
-                var reader = new BcrReader(log, config);
-                var filter = new BcrFilter(bcrOptions);
-                var writer = new Excel();
-                var pathProvider = new PathProvider(bcrOptions);
-                return new BcrReportRunner(log, reader, filter, writer, pathProvider);
+                return CreateBcrRunner(bcrOptions);
             }
 
             var configOptions = options as ConfigOptions;
             if (configOptions != null)
             {
-                return new ConfigRunner(configOptions, file);
+                return new ConfigRunner(configOptions, _file);
             }
 
             return new NullRunner();
+        }
+
+        private IRunner CreateBcrRunner(BcrOptions bcrOptions)
+        {
+            var log = new Logging();
+            var config = _file.Exists() ? _file.Load() : new ConfigOptions();
+            var reader = new BcrReader(log, config);
+            var filter = new BcrFilter(bcrOptions);
+            var writer = new Excel();
+            var pathProvider = new PathProvider(bcrOptions);
+            return new BcrReportRunner(log, reader, filter, writer, pathProvider);
         }
     }
 }
