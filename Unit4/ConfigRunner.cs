6296cf8c0125a133b445456b8bbb59b5cef5d0df
Tom Smith <tom.smith@cambridgeshire.gov.uk>
2018-06-27 11:18:46 +0100

    Red: given config options, use the config runner

---
+++ Unit4/ConfigRunner.cs
@@ -0,0 +1,13 @@
+using System;
+using Unit4.Automation.Interfaces;
+
+namespace Unit4.Automation
+{
+    internal class ConfigRunner : IRunner
+    {
+        public void Run()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
