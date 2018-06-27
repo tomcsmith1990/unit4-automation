bef37cd9f95433408e81f449ac9a77def022f113
Tom Smith <tom.smith@cambridgeshire.gov.uk>
2018-06-27 12:09:30 +0100

    Check the other combinations of old/new options

--- Unit4.Automation.Tests/ConfigRunnerTests.cs
+++ Unit4.Automation.Tests/ConfigRunnerTests.cs
@@ -36,5 +36,39 @@ namespace Unit4.Automation.Tests
 
             Assert.That(loadedOptions, Is.EqualTo(new ConfigOptions(9999, "http://test.url")));
         }
+
+        [Test]
+        public void GivenPriorOptionsAndNewUrl_ThenTheUnchangedOptionsShouldBePersisted()
+        {
+            var priorOptions = new ConfigOptions(1234, "http://test.url");
+            priorOptions.Save();
+
+            var options = new ConfigOptions(url: "http://some/other/test.url");
+
+            var runner = new ConfigRunner(options);
+
+            runner.Run();
+
+            var loadedOptions = ConfigOptions.Load();
+
+            Assert.That(loadedOptions, Is.EqualTo(new ConfigOptions(1234, "http://some/other/test.url")));
+        }
+
+        [Test]
+        public void GivenPriorOptionsAndAllNewOptions_ThenTheNewOptionsShouldBePersisted()
+        {
+            var priorOptions = new ConfigOptions(1234, "http://test.url");
+            priorOptions.Save();
+
+            var options = new ConfigOptions(9999, "http://some/other/test.url");
+
+            var runner = new ConfigRunner(options);
+
+            runner.Run();
+
+            var loadedOptions = ConfigOptions.Load();
+
+            Assert.That(loadedOptions, Is.EqualTo(new ConfigOptions(9999, "http://some/other/test.url")));
+        }
     }
 }
