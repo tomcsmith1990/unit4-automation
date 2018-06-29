ae5809f49f867de02680bf6824805d2785812e8f
Tom Smith <tom.smith@cambridgeshire.gov.uk>
2018-06-29 11:20:42 +0100

    Extract method

--- Unit4/ReportRunnerFactory.cs
+++ Unit4/ReportRunnerFactory.cs
@@ -37,13 +37,18 @@ namespace Unit4.Automation
         private IRunner CreateBcrRunner(BcrOptions bcrOptions)
         {
             var log = new Logging();
-            var config = _file.Exists() ? _file.Load() : new ConfigOptions();
-            var programConfig = new ProgramConfig(config.Client, config.Url);
-            var reader = new BcrReader(log, programConfig);
+            var config = GetConfig();
+            var reader = new BcrReader(log, config);
             var filter = new BcrFilter(bcrOptions);
             var writer = new Excel();
             var pathProvider = new PathProvider(bcrOptions);
             return new BcrReportRunner(log, reader, filter, writer, pathProvider);
+        }
+
+        private ProgramConfig GetConfig()
+        {
+            var config = _file.Exists() ? _file.Load() : new ConfigOptions();
+            return new ProgramConfig(config.Client, config.Url);
         }
     }
 }
