b9dedc9d1a30aca4f991639777aba8ca9d31d83b
Tom Smith <tom.smith@cambridgeshire.gov.uk>
2018-07-02 11:56:30 +0100

    Red: check we return the cached and the fetched

--- Unit4.Automation.Tests/BcrReaderTests.cs
+++ Unit4.Automation.Tests/BcrReaderTests.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using Unit4.Automation.Commands.BcrCommand;
 using System.Collections.Generic;
 using Unit4.Automation.Tests.Helpers;
+using System.Data;
 
 namespace Unit4.Automation.Tests
 {
@@ -17,6 +18,7 @@ namespace Unit4.Automation.Tests
         public void GivenNoCache_ThenItShouldFetchAllTier3Hierarchies()
         {
             var engine = new Mock<IUnit4Engine>();
+            engine.Setup(x => x.RunReport(Resql.BcrTier3("tier3"))).Returns(CreateDataSet("tier3"));
             var reader = CreateReader(new [] { new CostCentre() { Tier3 = "tier3" } }, new BcrOptions(), Mock.Of<IFile<Bcr>>(), engine.Object);
 
             reader.Read();
@@ -28,10 +30,11 @@ namespace Unit4.Automation.Tests
         public void GivenTier3WithAllCostCentresCached_ThenItShouldNotFetchThatTier3()
         {
             var engine = new Mock<IUnit4Engine>();
+
             var bcrCache = new Mock<IFile<Bcr>>();
             bcrCache.Setup(x => x.Exists()).Returns(true);
             bcrCache.Setup(x => x.IsDirty()).Returns(false);
-            bcrCache.Setup(x => x.Read()).Returns(new Bcr(new BcrLine[] { A.BcrLine().With(A.Criteria.Tier3, "tier3") }));
+            bcrCache.Setup(x => x.Read()).Returns(new Bcr(new BcrLine[] { A.BcrLine().With(A.Criteria.Tier3, "tier3").Build() }));
 
             var reader = CreateReader(new [] { new CostCentre() { Tier3 = "tier3" } }, new BcrOptions(), bcrCache.Object, engine.Object);
 
@@ -44,17 +47,21 @@ namespace Unit4.Automation.Tests
         public void GivenTier3WithOneCostCentresCached_ThenItShouldOnlyFetchTheUncachedTier3()
         {
             var engine = new Mock<IUnit4Engine>();
+            engine.Setup(x => x.RunReport(Resql.BcrTier3("b"))).Returns(CreateDataSet("b"));
+
             var bcrCache = new Mock<IFile<Bcr>>();
             bcrCache.Setup(x => x.Exists()).Returns(true);
             bcrCache.Setup(x => x.IsDirty()).Returns(false);
-            bcrCache.Setup(x => x.Read()).Returns(new Bcr(new BcrLine[] { A.BcrLine().With(A.Criteria.Tier3, "a") }));
+            bcrCache.Setup(x => x.Read()).Returns(new Bcr(new BcrLine[] { A.BcrLine().With(A.Criteria.Tier3, "a").Build() }));
 
             var reader = CreateReader(new [] { new CostCentre() { Tier3 = "a" }, new CostCentre() { Tier3 = "b" } }, new BcrOptions(), bcrCache.Object, engine.Object);
 
-            reader.Read();
+            var tier3sInBcr = reader.Read().Lines.Select(x => x.CostCentre.Tier3);
 
             engine.Verify(x => x.RunReport(Resql.BcrTier3("b")), Times.Once);
             engine.Verify(x => x.RunReport(Resql.BcrTier3("a")), Times.Never);
+
+            Assert.That(tier3sInBcr, Is.EquivalentTo(new [] { "a", "b" }));
         }
 
         private BcrReader CreateReader(IEnumerable<CostCentre> allCostCentres, BcrOptions options, IFile<Bcr> bcrCache, IUnit4Engine engine)
@@ -74,6 +81,34 @@ namespace Unit4.Automation.Tests
                 Mock.Of<IFile<SerializableCostCentreList>>(),
                 factory.Object,
                 costCentresProvider.Object);
+        }
+
+        private DataSet CreateDataSet(string tier3)
+        {
+            var dataset = new DataSet();
+            var table = dataset.Tables.Add("foo");
+            table.Columns.Add("r0r0r0r3dim2", typeof(string));
+            table.Columns.Add("r0r0r3dim2", typeof(string));
+            table.Columns.Add("r0r3dim2", typeof(string));
+            table.Columns.Add("r3dim2", typeof(string));
+            table.Columns.Add("dim2", typeof(string));
+            table.Columns.Add("xr0r0r0r3dim2", typeof(string));
+            table.Columns.Add("xr0r0r3dim2", typeof(string));
+            table.Columns.Add("xr0r3dim2", typeof(string));
+            table.Columns.Add("xr3dim2", typeof(string));
+            table.Columns.Add("xdim2", typeof(string));
+            table.Columns.Add("dim1", typeof(string));
+            table.Columns.Add("xdim1", typeof(string));
+            table.Columns.Add("plb_amount", typeof(double));
+            table.Columns.Add("f0_budget_to_da13", typeof(double));
+            table.Columns.Add("f1_total_exp_to16", typeof(double));
+            table.Columns.Add("f3_variance_to_15", typeof(double));
+            table.Columns.Add("plf_amount", typeof(double));
+            table.Columns.Add("f2_outturn_vari18", typeof(double));
+
+            var row = table.NewRow();
+            table.Rows.Add(string.Empty, string.Empty, tier3, string.Empty, string.Empty, string.Empty, string.Empty, string.Empty, string.Empty, string.Empty, string.Empty, string.Empty, 0, 0, 0, 0, 0, 0);
+            return dataset;
         }
     }
 }
