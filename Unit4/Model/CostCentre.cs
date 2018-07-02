3aee5cad78cb216c9ac3a28b1138b80a45927dc0
Tom Smith <tom.smith@cambridgeshire.gov.uk>
2018-07-02 13:44:40 +0100

    Override Equals and GetHashCode()

--- Unit4/Model/CostCentre.cs
+++ Unit4/Model/CostCentre.cs
@@ -13,5 +13,45 @@ namespace Unit4.Automation.Model
         public string Tier3Name { get; set; }
         public string Tier4Name { get; set; }
         public string CostCentreName { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            var other = obj as CostCentre;
+            if (other == null)
+            {
+                return false;
+            }
+
+            return Tier1 == other.Tier1 &&
+                Tier2 == other.Tier2 &&
+                Tier3 == other.Tier3 &&
+                Tier4 == other.Tier4 &&
+                Code == other.Code &&
+                Tier1Name == other.Tier1Name &&
+                Tier2Name == other.Tier2Name &&
+                Tier3Name == other.Tier3Name &&
+                Tier4Name == other.Tier4Name &&
+                CostCentreName == other.CostCentreName;
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                int hash = (int) 2166136261;
+                hash = hash * 16777619 + Tier1.GetHashCode();
+                hash = hash * 16777619 + Tier2.GetHashCode();
+                hash = hash * 16777619 + Tier3.GetHashCode();
+                hash = hash * 16777619 + Tier4.GetHashCode();
+                hash = hash * 16777619 + Code.GetHashCode();
+                hash = hash * 16777619 + Tier1Name.GetHashCode();
+                hash = hash * 16777619 + Tier2Name.GetHashCode();
+                hash = hash * 16777619 + Tier3Name.GetHashCode();
+                hash = hash * 16777619 + Tier4Name.GetHashCode();
+                hash = hash * 16777619 + CostCentreName.GetHashCode();
+                return hash;
+            }
+        }
+
     }
 }
