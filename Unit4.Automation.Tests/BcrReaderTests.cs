df59f8123ccfcf9247054cd25a5663015aace042
Tom Smith <tom.smith@cambridgeshire.gov.uk>
2018-07-02 10:55:13 +0100

    Extract factory method

--- Unit4.Automation.Tests/BcrReaderTests.cs
+++ Unit4.Automation.Tests/BcrReaderTests.cs
@@ -5,6 +5,7 @@ using Moq;
 using System.IO;
 using System.Linq;
 using Unit4.Automation.Commands.BcrCommand;
+using System.Collections.Generic;
 
 namespace Unit4.Automation.Tests
 {
@@ -15,27 +16,31 @@ namespace Unit4.Automation.Tests
         public void GivenNoCache_ThenItShouldFetchAllTier3Hierarchies()
         {
             var engine = new Mock<IUnit4Engine>();
-
-            var factory = new Mock<IUnit4EngineFactory>();
-            factory.Setup(x => x.Create()).Returns(engine.Object);
-
-            var costCentresProvider = new Mock<ICostCentresProvider>();
-            var costCentres = new SerializableCostCentreList() { CostCentres = new [] { new CostCentre() { Tier3 = "tier3" } } };
-            costCentresProvider.Setup(x => x.GetCostCentres()).Returns(costCentres);
-
-            var reader = 
-                new BcrReader(
-                    Mock.Of<ILogging>(),
-                    new BcrOptions(),
-                    new ProgramConfig(() => 0, () => string.Empty),
-                    Mock.Of<IFile<Bcr>>(),
-                    Mock.Of<IFile<SerializableCostCentreList>>(),
-                    factory.Object,
-                    costCentresProvider.Object);
+            var reader = CreateReader(new [] { new CostCentre() { Tier3 = "tier3" } }, new BcrOptions(), Mock.Of<IFile<Bcr>>(), engine.Object);
 
             reader.Read();
 
             engine.Verify(x => x.RunReport(Resql.BcrTier3("tier3")), Times.Once);
+        }
+
+
+        private BcrReader CreateReader(IEnumerable<CostCentre> allCostCentres, BcrOptions options, IFile<Bcr> bcrCache, IUnit4Engine engine)
+        {
+            var factory = new Mock<IUnit4EngineFactory>();
+            factory.Setup(x => x.Create()).Returns(engine);
+
+            var costCentresProvider = new Mock<ICostCentresProvider>();
+            var costCentres = new SerializableCostCentreList() { CostCentres = allCostCentres };
+            costCentresProvider.Setup(x => x.GetCostCentres()).Returns(costCentres);
+
+            return new BcrReader(
+                Mock.Of<ILogging>(),
+                options,
+                new ProgramConfig(() => 0, () => string.Empty),
+                bcrCache,
+                Mock.Of<IFile<SerializableCostCentreList>>(),
+                factory.Object,
+                costCentresProvider.Object);
         }
     }
 }
