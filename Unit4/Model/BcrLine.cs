ff61500c02c4e948f8f4d95178ee89137b18cb8f
Tom Smith <tom.smith@cambridgeshire.gov.uk>
2018-07-02 13:47:03 +0100

    Override Equals and GetHashCode

--- Unit4/Model/BcrLine.cs
+++ Unit4/Model/BcrLine.cs
@@ -13,5 +13,42 @@ namespace Unit4.Automation.Model
         public double Variance { get; set; }
         public double Forecast { get; set; }
         public double OutturnVariance { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            var other = obj as BcrLine;
+            if (other == null)
+            {
+                return false;
+            }
+
+            return CostCentre == other.CostCentre &&
+                Account == other.Account &&
+                AccountName == other.AccountName &&
+                Budget == other.Budget &&
+                Profile == other.Profile &&
+                Actuals == other.Actuals &&
+                Variance == other.Variance &&
+                Forecast == other.Forecast &&
+                OutturnVariance == other.OutturnVariance;
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                int hash = (int) 2166136261;
+                hash = hash * 16777619 + CostCentre.GetHashCode();
+                hash = hash * 16777619 + Account.GetHashCode();
+                hash = hash * 16777619 + AccountName.GetHashCode();
+                hash = hash * 16777619 + Budget.GetHashCode();
+                hash = hash * 16777619 + Profile.GetHashCode();
+                hash = hash * 16777619 + Actuals.GetHashCode();
+                hash = hash * 16777619 + Variance.GetHashCode();
+                hash = hash * 16777619 + Forecast.GetHashCode();
+                hash = hash * 16777619 + OutturnVariance.GetHashCode();
+                return hash;
+            }
+        }
     }
 }
