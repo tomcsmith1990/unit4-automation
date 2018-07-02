9df08ea37f6ae513ce72ca3d67cf0e7a18c87f17
Tom Smith <tom.smith@cambridgeshire.gov.uk>
2018-07-02 10:06:04 +0100

    Green: match the cost centres against the options before grouping

--- Unit4/CostCentreHierarchy.cs
+++ Unit4/CostCentreHierarchy.cs
@@ -2,22 +2,26 @@ using System.Collections.Generic;
 using System.Linq;
 using Unit4.Automation.Interfaces;
 using Unit4.Automation.Model;
+using Unit4.Automation.Commands.BcrCommand;
 
 namespace Unit4.Automation
 {
     internal class CostCentreHierarchy
     {
         private readonly ICache<SerializableCostCentreList> _costCentres;
+        private readonly BcrOptions _options;
 
         public CostCentreHierarchy(ICache<SerializableCostCentreList> costCentres, BcrOptions options)
         {
             _costCentres = costCentres;
+            _options = options;
         }
 
         public IEnumerable<IGrouping<string, CostCentre>> GetHierarchyByTier3()
         {
             var costCentres = _costCentres.Fetch().CostCentres;
-            return costCentres.GroupBy(x => x.Tier3, x => x);
+            var filteredCostCentres = costCentres.Where(x => x.Matches(_options));
+            return filteredCostCentres.GroupBy(x => x.Tier3, x => x);
         }
     }
 }
