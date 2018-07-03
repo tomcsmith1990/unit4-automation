2691816d5a3dbbcf13a454da55f99f93e56b5d61
Tom Smith <tom.smith@cambridgeshire.gov.uk>
2018-07-03 13:41:02 +0100

    Pass major.minor.build into the script

--- version.ps1
+++ version.ps1
@@ -1,16 +1,19 @@
-function Update-Build([int] $build, [string] $assemblyInfoPath) {
+function Update-Version([string] $version, [string] $assemblyInfoPath) {
     $tmpFile = "AssemblyInfo.cs"
 
     Get-Content $assemblyInfoPath | 
-    %{ $_ -replace "(Assembly.*Version)\(""([0-9]+).([0-9]+).([0-9]+).[0-9]+""\)", "`$1(""`$2.`$3.`$4.$build"")" } |
+    %{ $_ -replace "(Assembly.*Version)\(""[0-9]+.[0-9]+.[0-9]+.[0-9]+""\)", "`$1(""$version"")" } |
     Set-Content $tmpFile
 
     Move-Item $tmpFile $assemblyInfoPath -Force
 }
 
-function Set-Build {
-    $build = $env:TRAVIS_BUILD_NUMBER
-    Get-ChildItem -Path AssemblyInfo.cs -Recurse | ForEach-Object { Update-Build $build $_ }
+function Set-Version ([string] $version) {
+    $build = if (Test-Path env:TRAVIS_BUILD_NUMBER) { $env:TRAVIS_BUILD_NUMBER } else { 0 }
+    
+    $assemblyVersion = "$version.$build"
+
+    Get-ChildItem -Path AssemblyInfo.cs -Recurse | ForEach-Object { Update-Version $assemblyVersion $_ }
 }
 
-Set-Build
+Set-Version "1.0.0"
