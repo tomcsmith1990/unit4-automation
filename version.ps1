9665898b5dd881a9c463ff35d914fccdf7d234e0
Tom Smith <tom.smith@cambridgeshire.gov.uk>
2018-07-03 15:27:36 +0100

    Update the installer version

--- version.ps1
+++ version.ps1
@@ -8,6 +8,18 @@ function Update-Version([string] $version, [string] $assemblyInfoPath) {
     Move-Item $tmpFile $assemblyInfoPath -Force
 }
 
+function Update-InstallerVersion([string] $version) {
+    $tmpFile = "installer.wxs"
+
+    $path = ".\unit4-automation.wxs"
+
+    Get-Content $path | 
+    % { $_ -replace "\<\?define ProductVersion=""1.0.0""\?\>", "<?define ProductVersion=""$version""?>" } |
+    Set-Content $tmpFile
+
+    Move-Item $tmpFile $path -Force
+}
+
 function Set-Version () {
     $version = if (Test-Path env:UNIT4_AUTOMATION_VERSION) { $env:UNIT4_AUTOMATION_VERSION } else { "1.0.0" }
     $build = if (Test-Path env:TRAVIS_BUILD_NUMBER) { $env:TRAVIS_BUILD_NUMBER } else { 0 }
@@ -15,6 +27,8 @@ function Set-Version () {
     $assemblyVersion = "$version.$build"
     
     Get-ChildItem -Path AssemblyInfo.cs -Recurse | ForEach-Object { Update-Version $assemblyVersion $_ }
+
+    Update-InstallerVersion $version
 }
 
 Set-Version
