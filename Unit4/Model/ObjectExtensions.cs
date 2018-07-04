c0007a648af82409f2217e82e9ad48eb6b082e02
Tom Smith <tom.smith@cambridgeshire.gov.uk>
2018-07-04 13:41:40 +0100

    Extract extension method

---
+++ Unit4/Model/ObjectExtensions.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Unit4.Automation.Model
+{
+    internal static class ObjectExtensions
+    {
+        public static int NullSafeCompareTo<T>(this T a, T b) where T : System.IComparable<T>
+        {
+            if (a == null && b == null) return 0;
+            if (a == null) return -1;
+            if (b == null) return 1;
+            return a.CompareTo(b);
+        }
+    }
+}
