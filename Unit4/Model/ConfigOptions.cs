305e3958f056bb950d5a20557f821a8ff9317238
Tom Smith <tom.smith@cambridgeshire.gov.uk>
2018-07-06 10:47:27 +0100

    Use autoproperty

--- Unit4/Model/ConfigOptions.cs
+++ Unit4/Model/ConfigOptions.cs
@@ -6,22 +6,17 @@ namespace Unit4.Automation.Model
     [Verb("config", HelpText = "Configure the Unit4 connection details.")]
     internal class ConfigOptions : IOptions
     {
-        private readonly string _url;
-
         public ConfigOptions(int client = 0, string url = null)
         {
             Client = client;
-            _url = url;
+            Url = url;
         }
 
         [Option(HelpText = "Set the Unit4 client.")]
         public int Client { get; }
 
         [Option(HelpText = "Set the Unit4 SOAP service URL.")]
-        public string Url
-        {
-            get { return _url; }
-        }
+        public string Url { get; }
 
         public override bool Equals(object o)
         {
