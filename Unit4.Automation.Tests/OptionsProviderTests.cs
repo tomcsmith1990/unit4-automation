e260cc441ce617d1f455e999746493d5857e3922
Tom Smith <tom.smith@cambridgeshire.gov.uk>
2018-07-06 14:39:19 +0100

    Check that we can load each type of options (and that each is exported)

--- Unit4.Automation.Tests/OptionsProviderTests.cs
+++ Unit4.Automation.Tests/OptionsProviderTests.cs
@@ -3,12 +3,21 @@ using Unit4.Automation;
 using Unit4.Automation.Interfaces;
 using System.ComponentModel;
 using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+using System;
+using Unit4.Automation.Model;
 
 namespace Unit4.Automation.Tests
 {
     [TestFixture]
     internal class OptionsProviderTests
     {
+        [SetUp]
+        public void Setup()
+        {
+            _options = null;
+        }
+
         [Test]
         public void GivenAnExportOfOptionsInAssembly_ThenItShouldBeLoaded()
         {
@@ -16,6 +25,21 @@ namespace Unit4.Automation.Tests
 
             Assert.That(provider.Types, Is.EqualTo(new[] { typeof(FakeOptions) }));
         }
+
+        [TestCase(typeof(BcrOptions))]
+        [TestCase(typeof(ConfigOptions))]
+        public void GivenAnImportOfOptions_ThenWeShouldLoadTheType(Type type)
+        {
+            var catalog = new AggregateCatalog();
+            catalog.Catalogs.Add(new TypeCatalog(new[] { type }));
+            var container = new CompositionContainer(catalog);
+
+            container.ComposeParts(this);
+
+            Assert.That(_options, Is.TypeOf(type));
+        }
+
+        [Import(typeof(IOptions))] private IOptions _options;
 
         [Export(typeof(IOptions))]
         private class FakeOptions : IOptions
